1ca3e331f23adfeddc85f99c833bb2cc28ba831c
WangChen <diudiu_WangChen@163.com>
2020-11-03 09:00:34 +0800

    适配iOS14

--- CWCarouselDemo/CWCarousel/Swift/CWBanner.swift
+++ CWCarouselDemo/CWCarousel/Swift/CWBanner.swift
@@ -375,7 +375,11 @@ extension CWBanner {
 extension CWBanner {
     /// 开始拖拽
     func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
-        self.banner.isPagingEnabled = true
+        if #available(iOS 14.0, *) {
+            self.banner.isPagingEnabled = false
+        } else {
+            self.banner.isPagingEnabled = true
+        }
         if self.autoPlay {
             self.pause()
         }
